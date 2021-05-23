671ace049f227f436f41c4feef0c8c0bf6055269
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2021-05-23 12:14:44 +0530

    Add files via upload

---
+++ links/Day-1/CurrentWeather/app.js
@@ -0,0 +1,61 @@
+window.addEventListener("load", ()=>{
+    let lat;
+    let long;
+    let currentTemp=document.querySelector(".temp-current");
+    let messageTemp=document.querySelector(".temp-message");
+    let locationTimeZone=document.querySelector(".location-timezone");
+    let locationIcon=document.querySelector(".weather-image");
+    let tempReading=document.querySelector(".temp");
+    let tempSpan=document.querySelector(".temp span");
+    if(navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition( position=>{
+            lat=position.coords.latitude;
+            long=position.coords.longitude;
+        
+            const api=`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=ce811ff318b11fbdb7d7ee311d171e43`;
+
+            fetch(api)
+                .then(response=>{
+                      return response.json();
+                 })
+                .then(data=>{
+                       console.log(data);
+                       currentTemp.textContent=Math.floor((data.main.temp-273.15)*9/5+32);
+                       messageTemp.textContent=data.weather[0].description;
+                       locationTimeZone.textContent=data.name;
+                       locationIcon.src =`animated/${data.weather[0].icon}.svg`;
+
+                       let fahrenheit= currentTemp.textContent; 
+                       let  celsius = (currentTemp.textContent-32)*(5/9); 
+                        
+                       temperatureReading(tempReading,celsius,fahrenheit);
+                });
+                
+                
+            
+        });
+        
+    }
+    else
+        window.alert("Please give access to your location to work");
+    
+    
+    function temperatureReading(tempReading,celsius,fahrenheit)
+    {
+        
+        tempReading.addEventListener("click", ()=>{
+            if(tempSpan.textContent==="°F")
+            {
+                currentTemp.textContent=Math.floor(celsius);
+                tempSpan.textContent="°C";
+            }            
+            else
+            {
+                currentTemp.textContent=Math.floor(fahrenheit);
+                tempSpan.textContent="°F";
+            }
+        })
+    }
+
+})
--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Competitive Programmer","Explorer"],
+        strings: ["Developer", "Competitive Programmer","Explorer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Competitive Programmer","Explorer"],
+        strings: ["Developer", "Competitive Programmer","Explorer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
